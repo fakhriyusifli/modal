e160e175dd52f13f1b22c9b1cdab2c68d131ffe1
Fakhri Yusifli <fakhriyusifli@gmail.com>
2020-11-13 00:05:57 +0400

    open modal btns

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
